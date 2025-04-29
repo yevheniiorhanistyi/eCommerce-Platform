4acf05061e5e68d61a30c5b639b0e4f93d8e991b
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-04-29 11:04:02 +0200

    chore: set up folder structure and add basic page components

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const About = (): JSX.Element => {
+  return (
+    <main className="flex items-center justify-center min-h-screen p-8 sm:p-20">
+      <h1 className="text-2xl font-bold">About Page</h1>
+    </main>
+  );
+};
+
+export default About;
