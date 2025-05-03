322d3ec9fc549f525b5d6fdf007f5e7f16576c07
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-03 09:33:16 +0200

    refactor: wrap Header content in a max-width container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -58,153 +58,156 @@ const Header = (): JSX.Element => {
   }, [isOpen]);
 
   return (
-    <header className="flex items-center justify-between px-5 sm:px-10 py-4 grow-0 shrink-0 basis-auto">
-      <Link className="flex items-center gap-2" href={'/'}>
-        <Image
-          className="h-[35px] w-[17px] sm:h-[51px] sm:w-[25px]"
-          src="/logo.png"
-          width={25}
-          height={51}
-          alt="Logo"
-        />
-        <h1 className="text-2xl sm:text-3xl tracking-wide font-[family-name:var(--font-mr-dafoe)]">
-          StepUp
-        </h1>
-      </Link>
-      <NavigationMenu>
-        <NavigationMenuList>
-          {navLinks.map(({ href, label }) => (
-            <NavigationMenuItem key={href} className="hidden sm:flex">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                <Link className="sm:text-lg" href={href}>
-                  {label}
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          ))}
-          <NavigationMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  aria-label="User menu"
-                  className="rounded-full cursor-pointer transition-colors duration-300"
-                  variant="ghost"
-                  size="icon"
-                >
-                  <CircleUserRound className="size-6" strokeWidth={1.6} />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem asChild>
-                  <Link className="flex items-center gap-2 w-full cursor-pointer" href={'/login'}>
-                    <KeyRound />
-                    <span className="text-base">Sign In</span>
+    <header className="px-5 sm:px-10 py-4 grow-0 shrink-0 basis-auto">
+      <div className="flex items-center justify-between w-full max-w-[1440px] mx-auto">
+        <Link className="flex items-center gap-2" href={'/'}>
+          <Image
+            className="h-[35px] w-[17px] sm:h-[51px] sm:w-[25px]"
+            src="/logo.png"
+            width={25}
+            height={51}
+            alt="Logo"
+          />
+          <h1 className="text-2xl sm:text-3xl tracking-wide font-[family-name:var(--font-mr-dafoe)]">
+            StepUp
+          </h1>
+        </Link>
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navLinks.map(({ href, label }) => (
+              <NavigationMenuItem key={href} className="hidden sm:flex">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                  <Link className="sm:text-lg" href={href}>
+                    {label}
                   </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <Link
-                    className="flex items-center gap-2 w-full cursor-pointer"
-                    href={'/register'}
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+            <NavigationMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    aria-label="User menu"
+                    className="rounded-full cursor-pointer transition-colors duration-300"
+                    variant="ghost"
+                    size="icon"
                   >
-                    <UserRoundPlus />
-                    <span className="text-base">Join Us</span>
-                  </Link>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href={'/cart'}>
-              <Button
-                aria-label="Cart"
-                className="rounded-full cursor-pointer transition-colors duration-300"
-                variant="ghost"
-                size="icon"
-              >
-                <ShoppingBasket className="size-6" strokeWidth={1.6} />
-              </Button>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem className="sm:hidden">
-            <Drawer open={isOpen} onOpenChange={setIsOpen} direction="right">
-              <DrawerTrigger asChild>
-                <Button
-                  aria-label="Open menu"
-                  className="rounded-full cursor-pointer transition-colors duration-300"
-                  variant="ghost"
-                  size="icon"
-                >
-                  <Menu className="size-6" strokeWidth={1.6} />
-                </Button>
-              </DrawerTrigger>
-              <DrawerContent>
-                <DrawerHeader className="flex items-end">
-                  <DrawerTitle>
-                    <VisuallyHidden>Menu</VisuallyHidden>
-                  </DrawerTitle>
-                  <DrawerClose asChild>
-                    <Button
-                      aria-label="Close menu"
-                      className="rounded-full cursor-pointer transition-colors duration-300"
-                      variant="ghost"
-                      size="icon"
-                    >
-                      <X className="size-6" strokeWidth={1.6} />
-                    </Button>
-                  </DrawerClose>
-                </DrawerHeader>
-                <div className="flex flex-col gap-5 p-5">
-                  <Link
-                    className="flex items-center justify-between w-full text-xl hover:text-accent-foreground/90"
-                    href={'/cart'}
-                    onClick={() => setIsOpen(false)}
-                  >
-                    Cart
-                    <ChevronRight />
-                  </Link>
-                  {navLinks.map(({ href, label }) => (
+                    <CircleUserRound className="size-6" strokeWidth={1.6} />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem asChild>
+                    <Link className="flex items-center gap-2 w-full cursor-pointer" href={'/login'}>
+                      <KeyRound />
+                      <span className="text-base">Sign In</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
                     <Link
-                      key={href}
+                      className="flex items-center gap-2 w-full cursor-pointer"
+                      href={'/register'}
+                    >
+                      <UserRoundPlus />
+                      <span className="text-base">Join Us</span>
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href={'/cart'}>
+                <Button
+                  aria-label="Cart"
+                  className="rounded-full cursor-pointer transition-colors duration-300"
+                  variant="ghost"
+                  size="icon"
+                >
+                  <ShoppingBasket className="size-6" strokeWidth={1.6} />
+                </Button>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="sm:hidden">
+              <Drawer open={isOpen} onOpenChange={setIsOpen} direction="right">
+                <DrawerTrigger asChild>
+                  <Button
+                    aria-label="Open menu"
+                    className="rounded-full cursor-pointer transition-colors duration-300"
+                    variant="ghost"
+                    size="icon"
+                  >
+                    <Menu className="size-6" strokeWidth={1.6} />
+                  </Button>
+                </DrawerTrigger>
+                <DrawerContent>
+                  <DrawerHeader className="flex items-end">
+                    <DrawerTitle>
+                      <VisuallyHidden>Menu</VisuallyHidden>
+                    </DrawerTitle>
+                    <DrawerClose asChild>
+                      <Button
+                        aria-label="Close menu"
+                        className="rounded-full cursor-pointer transition-colors duration-300"
+                        variant="ghost"
+                        size="icon"
+                      >
+                        <X className="size-6" strokeWidth={1.6} />
+                      </Button>
+                    </DrawerClose>
+                  </DrawerHeader>
+                  <div className="flex flex-col gap-5 p-5">
+                    <Link
                       className="flex items-center justify-between w-full text-xl hover:text-accent-foreground/90"
-                      href={href}
+                      href={'/cart'}
                       onClick={() => setIsOpen(false)}
                     >
-                      {label}
+                      Cart
                       <ChevronRight />
                     </Link>
-                  ))}
-                  <p className="text-lg text-accent-foreground/90 -tracking-tighter">
-                    Become a StepUp Member for the best products, inspiration and stories in sport.
-                  </p>
-                  <div className="flex items-center gap-4">
-                    <Link
-                      className="flex items-center gap-2"
-                      href={'/login'}
-                      onClick={() => setIsOpen(false)}
-                    >
-                      <Button className="text-base font-bold rounded-2xl cursor-pointer">
-                        Sign In
-                      </Button>
-                    </Link>
-                    <Link
-                      className="flex items-center gap-2"
-                      href={'/register'}
-                      onClick={() => setIsOpen(false)}
-                    >
-                      <Button
-                        variant="outline"
-                        className="text-base font-bold rounded-2xl cursor-pointer"
+                    {navLinks.map(({ href, label }) => (
+                      <Link
+                        key={href}
+                        className="flex items-center justify-between w-full text-xl hover:text-accent-foreground/90"
+                        href={href}
+                        onClick={() => setIsOpen(false)}
                       >
-                        Join Us
-                      </Button>
-                    </Link>
+                        {label}
+                        <ChevronRight />
+                      </Link>
+                    ))}
+                    <p className="text-lg text-accent-foreground/90 -tracking-tighter">
+                      Become a StepUp Member for the best products, inspiration and stories in
+                      sport.
+                    </p>
+                    <div className="flex items-center gap-4">
+                      <Link
+                        className="flex items-center gap-2"
+                        href={'/login'}
+                        onClick={() => setIsOpen(false)}
+                      >
+                        <Button className="text-base font-bold rounded-2xl cursor-pointer">
+                          Sign In
+                        </Button>
+                      </Link>
+                      <Link
+                        className="flex items-center gap-2"
+                        href={'/register'}
+                        onClick={() => setIsOpen(false)}
+                      >
+                        <Button
+                          variant="outline"
+                          className="text-base font-bold rounded-2xl cursor-pointer"
+                        >
+                          Join Us
+                        </Button>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              </DrawerContent>
-            </Drawer>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+                </DrawerContent>
+              </Drawer>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </header>
   );
 };
