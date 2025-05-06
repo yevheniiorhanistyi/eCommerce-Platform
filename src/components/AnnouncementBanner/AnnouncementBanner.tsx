81c65b9a7f8125b95162db418fbf50466ef70e0a
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-06 17:12:41 +0200

    feat: display social logos in AnnouncementBanner

--- src/components/AnnouncementBanner/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { socialLogos } from '@/constants/socials';
+
 interface AnnouncementBannerProps {
   label?: string;
   text: string[];
   socials?: boolean;
 }
 
-const AnnouncementBanner = ({ label, text }: AnnouncementBannerProps): JSX.Element => {
+const AnnouncementBanner = ({ label, text, socials }: AnnouncementBannerProps): JSX.Element => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 text-center text-xs sm:text-sm p-4 bg-neutral-950 text-amber-50">
       {label && <span className="font-bold">{label}</span>}
@@ -13,6 +17,23 @@ const AnnouncementBanner = ({ label, text }: AnnouncementBannerProps): JSX.Eleme
           <p key={line}>{line}</p>
         ))}
       </div>
+      {socials && (
+        <div className="flex flex-row items-center gap-5">
+          {socialLogos.map((logo) => (
+            <Link key={logo} href={'#'}>
+              <div className="relative w-[18px] h-[18px]">
+                <Image
+                  src={`/images/socials/${logo}`}
+                  alt={logo.replace('.png', '')}
+                  fill
+                  className="object-contain"
+                  sizes="18px"
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
