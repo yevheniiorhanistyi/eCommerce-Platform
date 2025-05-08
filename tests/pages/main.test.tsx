1853541cb81a62df174af87faea1556891f5af87
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-08 19:52:10 +0200

    test: update tests for Main page

--- tests/pages/main.test.tsx
+++ tests/pages/main.test.tsx
@@ -1,10 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import Main from '@/app/page';
 
-describe('MainPage', () => {
-  it('renders the main page correctly', () => {
+jest.mock('@/components/AnnouncementBanner/AnnouncementBanner', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>AnnouncementBanner</div>)
+}));
+
+jest.mock('@/components/HeroSection/HeroSection', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>HeroSection</div>)
+}));
+
+jest.mock('@/components/BrandCarousel/BrandCarousel', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>BrandCarousel</div>)
+}));
+
+jest.mock('@/components/PopularCategories/PopularCategories', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>PopularCategories</div>)
+}));
+
+jest.mock('@/components/AllTimeFavorites/AllTimeFavorites', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>AllTimeFavorites</div>)
+}));
+
+describe('Main', () => {
+  it('should render all components', () => {
     render(<Main />);
 
-    expect(screen.getByText(/Seasonal savings/i)).toBeInTheDocument();
+    expect(screen.getAllByText('AnnouncementBanner').length).toBe(3);
+    expect(screen.getByText('HeroSection')).toBeInTheDocument();
+    expect(screen.getByText('BrandCarousel')).toBeInTheDocument();
+    expect(screen.getByText('PopularCategories')).toBeInTheDocument();
+    expect(screen.getByText('AllTimeFavorites')).toBeInTheDocument();
   });
 });
