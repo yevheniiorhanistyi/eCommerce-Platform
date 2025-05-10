132e798a5dcc84ed7276198c47049e2ca3d00b7b
Dzmitry Drevich <jeidan2012@gmail.com>
2025-05-10 20:25:05 +0200

    refactor: split and modularize logic and types

---
+++ src/components/Register/types.ts
@@ -0,0 +1,46 @@
+import { FormikProps } from 'formik';
+
+type RegisterFormProps = Pick<
+  FormikProps<RegisterFormFields>,
+  'values' | 'errors' | 'touched' | 'handleChange' | 'handleBlur'
+> & { setFieldValue: (field: string, value: unknown) => void };
+
+type Address = {
+  country: string;
+  city: string;
+  street: string;
+  postalCode: string;
+  useSame?: boolean;
+  [key: string]: string | boolean | undefined;
+};
+
+type RegisterFormFields = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  phoneNumber: string;
+  billingAddress: Address;
+  shippingAddress: Address;
+};
+
+type CommonFormProps = {
+  name: string;
+  label: string;
+  type?: string;
+  placeholder?: string;
+  value: string | boolean | undefined;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onBlur: React.FocusEventHandler<HTMLInputElement>;
+  error?: string;
+  touched?: boolean;
+  withToggle?: boolean;
+  show?: boolean;
+  onToggle?: () => void;
+  withDatePicker?: boolean;
+  onDatePick?: (value: string) => void;
+};
+
+export type { RegisterFormProps, Address, RegisterFormFields, CommonFormProps };
