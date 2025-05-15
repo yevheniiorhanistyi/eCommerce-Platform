5c0b0f4ba0417aac76b9cca8bbe9b6bdafe8ca0e
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-15 07:33:04 +0200

    feat: update cookie check in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('access_token')?.value;
+  const isAuthenticated = request.cookies.get('is_authenticated')?.value === 'true';
   const { pathname } = request.nextUrl;
 
-  if (token && (pathname === '/login' || pathname === '/register')) {
+  if (isAuthenticated && (pathname === '/login' || pathname === '/register')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
